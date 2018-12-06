8ae4d18b38997b092c6427c1142412535b6abe33
FixTheInternet <vmpatoleia@protonmail.com>
2018-12-06 16:55:12 +0100

    Create layout component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Aux from '../../hoc/Auxiliary'
+
+const Layout = (props) => {
+    return (
+        <Aux>        
+        <div>
+            Toolbar, SideDrawer, Backdrop  
+        </div>
+        <main>
+            {props.children}
+        </main>
+        </Aux>
+
+    );
+};
+
+export default Layout;
---
+++ src/hoc/Auxiliary.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Aux = (props) => {
+    return (
+        props.children
+    );
+};
+
+export default Aux;
