27cf164ea3986b184aff04356c08f9dd35363e63
FixTheInternet <vmpatoleia@protonmail.com>
2018-12-07 15:36:26 +0100

    Output burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,15 +2,21 @@ import React from 'react';
 import Classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-const Burger = () => {
+const Burger = (props) => {
+
+    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+           return <BurgerIngredient key={igKey+i} type={igKey} />
+        })
+    });
+
     return (
         <div className={Classes.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="cheese"></BurgerIngredient>
-            <BurgerIngredient type="meat"></BurgerIngredient>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
     );
 };
 
-export default Burger;
+export default Burger; 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,21 @@ import Aux from './../../hoc/Auxiliary'
 import Burger from './../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salada: 1,
+            bacon: 1,
+            cheese: 1,
+            meat: 1
+        }
+    }
+
+
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Burger controls</div>
             </Aux>
         );
