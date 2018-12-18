610f291be771b1a4223bdb23a27ac271f569438c
FixTheInternet <vmpatoleia@protonmail.com>
2018-12-18 18:50:00 +0000

    Output multiple control options

--- src/components/Burger/BurgerIngredient/BuildControls/BuildControls.js
+++ src/components/Burger/BurgerIngredient/BuildControls/BuildControls.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+const controls = [
+    { label: "Salad", type: "salad"},
+    { label: "Bancon", type: "bacon"},
+    { label: "Cheese", type: "cheese"},
+    { label: "Meat", type: "meat"}
+]
+
 const BuildControls = () => {
     return (
         <div>
